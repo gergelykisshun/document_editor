20cedfd0e945dcfd3c8d467caf73135285f4f40d
gergely.kiss <gergely@hyperplane.hu>
2024-05-27 13:46:42 +0200

    Fix: Build to support top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    target: "es2022",
+  },
   optimizeDeps: {
-    exclude: ["pdfjs-dist"],
+    esbuildOptions: {
+      target: "es2022",
+    },
   },
 });
